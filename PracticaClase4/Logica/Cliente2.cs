43d38cfa0a75d33f636a551a9b1d1edb00a52276
Facukopech <facukopech@gmail.com>
2020-04-21 15:38:08 -0300

    no message

---
+++ PracticaClase4/Logica/Cliente2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logica;
+
+namespace Logica
+{
+    class Cliente2
+    {
+        public string Nombre { get; set; }
+        public int CUIT { get; set; }
+        public decimal Saldo { get; set; }
+
+
+
+    }
+}
